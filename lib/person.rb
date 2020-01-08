fcaa6f18364b8ff9bdc67a06f30a8dacde0fd6b2
Julianna Boyd <julianna.boyd91@gmail.com>
2020-01-08 18:40:09 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -11,10 +11,13 @@ class Person
   
   def happiness=(num) 
     @happiness = num 
+  if @happiness > 10 
+      @happiness = 10 
+  elsif
     @happiness = 10  if @happiness > 10 
     @happiness = 0 if @happiness < 0 
     @happiness = num if @happiness > 0 
-  
+  end 
   end 
   
   def hygiene=(num) 
